2cfb2fb502bdcebec664981f0bada187dc9537e5
Galdebar <Galdebar@gmail.com>
2020-06-05 15:59:12 +0300

    Initial commit of a blank Spring Boot project

---
+++ src/main/java/lt/galdebar/VilniusTemp/VilniusTempApplication.java
@@ -0,0 +1,13 @@
+package lt.galdebar.VilniusTemp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VilniusTempApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VilniusTempApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lt/galdebar/VilniusTemp/VilniusTempApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.galdebar.VilniusTemp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VilniusTempApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
